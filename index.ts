6cbf08a6644f4b20226da2c31b709169dd62b89f
amanmarakkar <amanahammedkt@gmail.com>
2023-11-18 16:42:31 +0530

    url issue4

--- index.ts
+++ index.ts
@@ -8,7 +8,8 @@ const app = express();
 const port = process.env.PORT || 3000
 const corsOptions ={
   origin:'http://localhost:5173', 
-  credentials:true,            //access-control-allow-credentials:true
+  credentials:true,
+  accessControlAllowCredentials:true,
   optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
